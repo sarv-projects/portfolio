dff62ec943efcbb633b9538113c3f9e66be7e5b5
sarvesh <sarveshbh.2022@gmail.com>
2026-03-03 20:41:31 +0000

    fix  issues

--- src/App.jsx
+++ src/App.jsx
@@ -23,7 +23,7 @@ const CONFIG = {
   skillGroups: [
     {
       label:"Languages",
-      skills:["python,javascript,c/c++,java"]
+      skills:["python","javascript","c/c++","java"]
     }
     ,
     {
